b2d28ea5fc48309d645bd80f4e66539845cce4e9
rusiruavb <rusiruavb98@gmail.com>
2022-05-29 10:16:22 +0530

    Add react icon library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h1>Mini Hackathon 2022</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Mini Hackathon 2022</h1>
+		</div>
+	);
 }
 
 export default App;
