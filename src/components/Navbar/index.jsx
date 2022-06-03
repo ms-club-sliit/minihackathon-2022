4399bfc21ba94998fd196ebb292d713583fcb6cf
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-06-03 13:48:15 +0530

    Spelling mistake in navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -81,7 +81,7 @@ const Header = () => {
             </li>
             <li className="text-base font-semibold text-gray-500 hover:text-gray-900 mt-1 list-none cursor-pointer">
               <Link to="gallery" spy={true} smooth={true} offset={50} duration={800} delay={100}>
-                Galley
+               Gallery
               </Link>
             </li>
             <li className="text-base font-semibold text-gray-500 hover:text-gray-900 mt-1 list-none cursor-pointer">
