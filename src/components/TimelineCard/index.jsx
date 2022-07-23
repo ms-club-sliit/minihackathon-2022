86afc838a3f3476939e3854d231add699172131b
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-07-23 07:13:44 +0530

    Changed the descriptions of Past winners

--- src/components/TimelineCard/index.jsx
+++ src/components/TimelineCard/index.jsx
@@ -31,10 +31,12 @@ const TimelineComponent = (props) => (
 			)}
 			{props.data.id === 3 && (
 				<a
-					href="/team/register"
+					//href="/team/register"
+					href="/notfound"
 					target="_blank"
 					rel="noopener noreferrer"
 					className="bg-white text-black border border-black hover:bg-gray-100 rounded lg:text-lg pl-4 pr-4 pt-1 pb-1 transition duration-0 hover:duration-500"
+			
 				>
 					✍🏼 Register your team
 				</a>
