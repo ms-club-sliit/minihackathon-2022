8c07f688c6d8a6217e279cf6fe0bd55288488663
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-07-27 09:51:42 +0530

    style(TeamCard) : fixed teamcard mobile responsive

--- src/components/TeamCard/index.jsx
+++ src/components/TeamCard/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const TeamCard = (props) => {
 	return (
-		<div className="md:w-1/2 lg:w-96 p-4 bg-white border-inherit h-52 border rounded-lg mb-5">
-			<div className="grid grid-rows-1 grid-cols-11 items-center p-4 pt-2">
+		<div className="md:w-1/3 md:ml-5 lg:w-96 p-4 bg-white border-inherit h-52 border rounded-lg mb-5">
+			<div className="grid md:flex md:flex-row md:justify-between grid-rows-1 grid-cols-11 items-center p-4 md:p-0 pt-2">
 				<div className="col-start-1 col-span-1">
 					<img
 						src={props.teamImage}
@@ -12,15 +12,15 @@ const TeamCard = (props) => {
 					/>
 				</div>
 				<div className="col-start-3 col-span-6">
-					<h1 className="font-bold">{props.team_name}</h1>
+					<p className="font-bold md:text-xs">{props.team_name}</p>
 				</div>
-				<div className="col-start-9 col-span-2">
-					<button className="w-24 whitespace-nowrap rounded-full border-2 px-3 py-1 text-base md:text-gray-800">
+				<div className="col-start-7 col-span-2">
+					<button className="w-24 md:w-20 whitespace-nowrap rounded-full md:text-xs border-2 px-3 py-1 text-base md:text-gray-800">
 						{props.round + "st Round"}
 					</button>
 				</div>
 			</div>
-			<div className="p-4 pt-0 pl-2">
+			<div className="p-4 pt-0 pl-2 md:pl-0">
 				<p className="text-gray-900 text-base font-medium mb-1">
 					{props.member01}
 				</p>
