db5ece4e9094649a2f39c9ffebb031cf2c3a3994
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-08-15 14:00:10 +0530

    Fixed the bug

--- src/views/Header/index.jsx
+++ src/views/Header/index.jsx
@@ -3,7 +3,7 @@ import CountdownTimer from "./CountdownTimer";
 import Logo from "../../assets/logo/mini_hackathon_logo.webp";
 
 const Countdown = () => {
-	const isAwarenessSession = false;
+	// const isAwarenessSession = false;
 	let myDate = "03-09-2022";
 	myDate = myDate.split("-");
 	let newDate = new Date(myDate[2], myDate[1] - 1, myDate[0]);
